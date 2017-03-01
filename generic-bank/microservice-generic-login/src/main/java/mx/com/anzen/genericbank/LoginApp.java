a7fc51bcb63feac9e94ebbcfe66a956cbc06a125
angelanzen <amartinez@anzen.com.mx>
2017-03-02 09:08:36 +1000

    Dummy para el API de persistencia mediante SP

--- generic-bank/microservice-generic-login/src/main/java/mx/com/anzen/genericbank/LoginApp.java
+++ generic-bank/microservice-generic-login/src/main/java/mx/com/anzen/genericbank/LoginApp.java
@@ -3,6 +3,7 @@ package mx.com.anzen.genericbank;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class LoginApp {
 
